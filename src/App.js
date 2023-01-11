17c3ff116576452c3ef8b69b53f2acc0f43db3bc
Rahul Kumar <rk2909559@gmail.com>
2023-01-11 13:28:15 +0530

    staging

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import vector from './img/Vectorvector.png'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <header>
+      <nav className='App'>
+        <h2> <img src={vector} alt="vector" /> Culinary kitchen</h2>
+      </nav>
+    </header>
+    <div className="gallary">
+      <h2>VARIETIES</h2>
+      <div className="gal">
+
+      </div>
+      <div className="gal2">
+        <div className="div1"></div>
+        <div className="div2"></div>
+        <div className="div3"></div>
+        <div className="div4"></div>
+      </div>
     </div>
+    </>
   );
 }
 
